09b9c62e7796aa216d0dc25c4584f9a1525ec7ee
DevAbdulrehman <ahmedrehman865@gmail.com>
2023-10-21 17:34:09 +0500

    all files uploaded

---
+++ scrpit.js
@@ -0,0 +1,24 @@
+function Add() {
+     num1 = parseInt(document.getElementById("num1").value);
+     num2 = parseInt(document.getElementById("num2").value);
+     document.getElementById("Result").innerText = num1 + num2;
+}
+
+function Subtract() {
+     num1 = parseInt(document.getElementById("num1").value);
+     num2 = parseInt(document.getElementById("num2").value);
+     document.getElementById("Result").innerText = num1 - num2;
+}
+
+function Multiply() {
+     num1 = parseInt(document.getElementById("num1").value);
+     num2 = parseInt(document.getElementById("num2").value);
+     document.getElementById("Result").innerText = num1*num2;
+}
+
+function Divide() {
+     num1 = parseInt(document.getElementById("num1").value);
+     num2 = parseInt(document.getElementById("num2").value);
+     document.getElementById("Result").innerText = num1/num2;
+}
+
