f1d301bb1cf79be8db79df6e8609d9b011dffb23
DevAbdulrehman <ahmedrehman865@gmail.com>
2023-11-07 10:17:03 +0500

    Minor changes made in code

--- scrpit.js
+++ scrpit.js
@@ -1,24 +1,31 @@
 function Add() {
-     num1 = parseInt(document.getElementById("num1").value);
-     num2 = parseInt(document.getElementById("num2").value);
-     document.getElementById("Result").innerText = num1 + num2;
+  num1 = parseInt(document.getElementById("num1").value);
+  num2 = parseInt(document.getElementById("num2").value);
+  document.getElementById("Result").innerText = num1 + num2;
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
 }
 
 function Subtract() {
-     num1 = parseInt(document.getElementById("num1").value);
-     num2 = parseInt(document.getElementById("num2").value);
-     document.getElementById("Result").innerText = num1 - num2;
+  num1 = parseInt(document.getElementById("num1").value);
+  num2 = parseInt(document.getElementById("num2").value);
+  document.getElementById("Result").innerText = num1 - num2;
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
 }
 
 function Multiply() {
-     num1 = parseInt(document.getElementById("num1").value);
-     num2 = parseInt(document.getElementById("num2").value);
-     document.getElementById("Result").innerText = num1*num2;
+  num1 = parseInt(document.getElementById("num1").value);
+  num2 = parseInt(document.getElementById("num2").value);
+  document.getElementById("Result").innerText = num1 * num2;
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
 }
 
 function Divide() {
-     num1 = parseInt(document.getElementById("num1").value);
-     num2 = parseInt(document.getElementById("num2").value);
-     document.getElementById("Result").innerText = num1/num2;
+  num1 = parseInt(document.getElementById("num1").value);
+  num2 = parseInt(document.getElementById("num2").value);
+  document.getElementById("Result").innerText = num1 / num2;
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
 }
-
